e383401a333f5efe3896dc9376ec87ab112a50f5
rachhtordoff <rachael.tordoff@hotmail.co.uk>
2022-03-08 10:42:39 +0000

    Update constants.py

--- local_land_charges_api_stub/constants/constants.py
+++ local_land_charges_api_stub/constants/constants.py
@@ -7,6 +7,6 @@ class AddChargeConstants(object):
     STATUTORY_PROVISION=[]
 
     url = 'https://search-local-land-charges.service.gov.uk/statutory-provisions'
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/605.1.15'}
     response = requests.get(url, headers=headers)
     STATUTORY_PROVISION=json.loads(response.content)
