0207a94e32b4a87ba75571b6d96730d4519b7dd3
rachhtord <rachael.tordoff@hotmail.co.uk>
2024-03-05 17:09:07 +0000

    added unittest folder

---
+++ unit_tests/test_health.py
@@ -0,0 +1,12 @@
+import unittest
+
+from local_land_charges_api_stub.main import app
+
+
+class TestHealth(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_health(self):
+        self.assertEqual((self.app.get('/health')).status_code, 200)
