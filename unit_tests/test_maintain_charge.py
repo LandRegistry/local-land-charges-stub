ba3d42447faa323f114b6d9e1b0baf4f3018120b
rachhtord <rachael.tordoff@hotmail.co.uk>
2024-03-07 13:44:57 +0000

    added more unit tests for updates

--- unit_tests/test_maintain_charge.py
+++ unit_tests/test_maintain_charge.py
@@ -205,3 +205,164 @@ class TestMaintainCharge(unittest.TestCase):
         data = json.loads(response.data)
         assert 'duplicate_charges' in data
 
+
+    def test_vary_charge_valid(self):
+        valid_land_charge_id = "LLC-01"
+        valid_version_id = "1"
+        payload = {
+            "item": {
+                "schema-version": "5.0",
+                "further-information-location": "some further info",
+                "charge-type": "Planning",
+                "charge-sub-category": "Conservation area",
+                "expiry-date": "2020-01-01",
+                "originating-authority": "Place City Council",
+                "charge-creation-date": "2017-01-12",
+                "geometry": {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                    "geometry": {
+                        "coordinates": [
+                        294300,
+                        21054
+                        ],
+                        "type": "Point"
+                    },
+                    "crs": {
+                        "type": "name1",
+                        "properties": {
+                        "name": "urn:ogc:def:crs:EPSG::27700"
+                        }
+                    },
+                    "type": "Feature",
+                    "properties": {
+                        "id": 410
+                    }
+                    }
+                ]
+                },
+                "statutory-provision": "Town and Country Planning Act 1990",
+                "further-information-reference": "AB1212",
+                "instrument": "Notice",
+                "charge-geographic-description": "Varying as LR user",
+                "supplementary-information": "a description of the local land charge"
+            }
+        }
+
+        response = self.client.put(
+            f"/v1.0/local-land-charges/{valid_land_charge_id}/{valid_version_id}",
+            data=json.dumps(payload),
+            content_type='application/json',
+            headers={'Accept': 'application/json'}
+        )
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_vary_charge_invalid_land_charge_id(self):
+        invalid_land_charge_id = "invalid123" 
+        valid_version_id = "1"
+        payload = {
+            "item": {
+                "schema-version": "5.0",
+                "further-information-location": "some further info",
+                "charge-type": "Planning",
+                "charge-sub-category": "Conservation area",
+                "expiry-date": "2020-01-01",
+                "originating-authority": "Place City Council",
+                "charge-creation-date": "2017-01-12",
+                "geometry": {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                    "geometry": {
+                        "coordinates": [
+                        294300,
+                        21054
+                        ],
+                        "type": "Point"
+                    },
+                    "crs": {
+                        "type": "name1",
+                        "properties": {
+                        "name": "urn:ogc:def:crs:EPSG::27700"
+                        }
+                    },
+                    "type": "Feature",
+                    "properties": {
+                        "id": 410
+                    }
+                    }
+                ]
+                },
+                "statutory-provision": "Town and Country Planning Act 1990",
+                "further-information-reference": "AB1212",
+                "instrument": "Notice",
+                "charge-geographic-description": "Varying as LR user",
+                "supplementary-information": "a description of the local land charge"
+            }
+        }
+
+
+        response = self.client.put(
+            f"/v1.0/local-land-charges/{invalid_land_charge_id}/{valid_version_id}",
+            data=json.dumps(payload),
+            content_type='application/json',
+            headers={'Accept': 'application/json'}
+        )
+
+        self.assertEqual(response.status_code, 422)
+
+    def test_vary_charge_invalid_version_id(self):
+        valid_land_charge_id = "LLC-01"
+        invalid_version_id = "abc"
+        payload = {
+                "item": {
+                    "schema-version": "5.0",
+                    "further-information-location": "some further info",
+                    "charge-type": "Planning",
+                    "charge-sub-category": "Conservation area",
+                    "expiry-date": "2020-01-01",
+                    "originating-authority": "Place City Council",
+                    "charge-creation-date": "2017-01-12",
+                    "geometry": {
+                    "type": "FeatureCollection",
+                    "features": [
+                        {
+                        "geometry": {
+                            "coordinates": [
+                            294300,
+                            21054
+                            ],
+                            "type": "Point"
+                        },
+                        "crs": {
+                            "type": "name1",
+                            "properties": {
+                            "name": "urn:ogc:def:crs:EPSG::27700"
+                            }
+                        },
+                        "type": "Feature",
+                        "properties": {
+                            "id": 410
+                        }
+                        }
+                    ]
+                    },
+                    "statutory-provision": "Town and Country Planning Act 1990",
+                    "further-information-reference": "AB1212",
+                    "instrument": "Notice",
+                    "charge-geographic-description": "Varying as LR user",
+                    "supplementary-information": "a description of the local land charge"
+                }
+            }
+
+
+        response = self.client.put(
+            f"/v1.0/local-land-charges/{valid_land_charge_id}/{invalid_version_id}",
+            data=json.dumps(payload),
+            content_type='application/json',
+            headers={'Accept': 'application/json'}
+        )
+
+        self.assertEqual(response.status_code, 422)
